00b36ebbb9baf452c9aa6a4a7ac8ff8e3f0af08d
Himadri Bhattacharjee <107522312+lavafroth@users.noreply.github.com>
2024-03-20 17:06:18 +0530

    feat: create april model struct

--- README.md
+++ README.md
@@ -2,5 +2,7 @@
 
 A pure Rust rewrite of the April ASR shared library.
 
+ONNX runtime is fetched automatically during compilation.
+
 Place the model file in the project directory with the name `model.april`.
 Now run `nix develop --command cargo run`.
--- src/main.rs
+++ src/main.rs
@@ -1,228 +1,87 @@
-use binread::{BinRead, BinReaderExt};
-use ort::{Environment, LoggingLevel};
-use std::{
-    error::Error,
-    fs,
-    io::{Read, Seek},
-};
+use ort::{Environment, InMemorySession, LoggingLevel};
+use std::{fs, sync::Arc};
+mod model_file;
 
 // finished porting the following:
 // params.{c,h}
 // model_file.{c,h}
+// april_model.{c,h}
+// ort_util.{c,h}
 
-// currently working on ort_util.c, april_model.c
-// WONTIMPLEMENT: transfer_strings_and_free_model
+// WONTIMPLEMENT: transfer_strings_and_free_model, *_free
+// WONTPORT:
+// init.c
+// log.h
+// proc_thread.{c,h}
 
-const MAX_NETWORKS: u64 = 8;
+// TODO:
+// april_session.c
+// common.h
+// fbank.c
+// audio_provider.c
 
-#[repr(C)]
-#[derive(BinRead)]
-pub struct ModelFileHeader {
-    // APRILMDL
-    aprilmdl: [u8; 8],
-    version: u32,
-    header_size: u64,
-}
+pub struct FBankOptions {
+    sample_freq: i32,
 
-#[repr(C)]
-#[derive(BinRead)]
-pub struct LanguageHeader {
-    language: [u8; 8],
-}
-
-#[derive(Debug, Clone, Copy, BinRead, PartialEq, Eq)]
-#[br(repr = u32)]
-pub enum ModelType {
-    ModelUnknown = 0,
-    ModelLstmTransducerStateless = 1,
-    ModelMax = 2,
-}
-
-pub fn read_string<T>(cursor: &mut T) -> Result<String, Box<dyn Error>>
-where
-    T: Read + Seek,
-{
-    let size: u64 = cursor.read_ne()?;
-    let mut buf = vec![0; size as usize];
-    cursor.read_exact(buf.as_mut_slice())?;
-    Ok(String::from_utf8(buf)?)
-}
-
-#[derive(Debug, Clone, Copy, BinRead)]
-pub struct ParamMetadata {
-    offset: u64,
-    size: u64,
-    num_networks: u64,
-}
-
-impl ParamMetadata {
-    pub fn parse<T>(self, cursor: &mut T) -> Result<Params, Box<dyn Error>>
-    where
-        T: Read + Seek,
-    {
-        assert!(self.num_networks <= MAX_NETWORKS);
-
-        let networks: Vec<Network> = (0..self.num_networks)
-            .map(|_| -> Result<Network, Box<dyn Error>> { Ok(cursor.read_ne()?) })
-            .collect::<Result<Vec<Network>, Box<dyn Error>>>()?;
-
-        cursor.seek(std::io::SeekFrom::Start(self.offset))?;
-        let partial_params: PartialParams = cursor.read_ne()?;
-
-        assert!(partial_params.batch_size == 1);
-        assert!((partial_params.segment_size > 0) && (partial_params.segment_size < 100));
-        assert!(
-            (partial_params.segment_step > 0)
-                && (partial_params.segment_step < 100)
-                && (partial_params.segment_step <= partial_params.segment_size)
-        );
-        assert!((partial_params.mel_features > 0) && (partial_params.mel_features < 256));
-        assert!((partial_params.sample_rate > 0) && (partial_params.sample_rate < 144000));
-        assert!((partial_params.token_count > 0) && (partial_params.token_count < 16384));
-        assert!(
-            (partial_params.blank_id >= 0)
-                && (partial_params.blank_id < partial_params.token_count)
-        );
-
-        assert!(
-            (partial_params.frame_shift_ms > 0)
-                && (partial_params.frame_shift_ms <= partial_params.frame_length_ms)
-        );
-        assert!((partial_params.frame_length_ms > 0) && (partial_params.frame_length_ms <= 5000));
-        assert!(
-            (partial_params.mel_low > 0) && (partial_params.mel_low < partial_params.sample_rate)
-        );
-        assert!(
-            (partial_params.mel_high == 0) || (partial_params.mel_high > partial_params.mel_low)
-        );
-
-        let mut params = Params {
-            batch_size: partial_params.batch_size,
-            segment_size: partial_params.segment_size,
-            segment_step: partial_params.segment_step,
-            mel_features: partial_params.mel_features,
-            sample_rate: partial_params.sample_rate,
-
-            frame_shift_ms: partial_params.frame_shift_ms,
-            frame_length_ms: partial_params.frame_length_ms,
-            round_pow2: partial_params.round_pow2 != 0,
-            mel_low: partial_params.mel_low,
-            mel_high: partial_params.mel_high,
-            snip_edges: partial_params.snip_edges != 0,
-
-            blank_id: partial_params.blank_id,
-            networks,
-            tokens: vec![],
-        };
-
-        params.tokens = Vec::with_capacity(partial_params.token_count as usize);
-
-        for _ in 0..partial_params.token_count {
-            let token_len: i32 = cursor.read_ne()?;
-            let mut buf = vec![0; token_len as usize];
-            cursor.read_exact(buf.as_mut_slice())?;
-            let token = std::str::from_utf8(&buf)?.trim_matches('\x00').to_string();
-            params.tokens.push(token);
-        }
-
-        Ok(params)
-    }
-}
-
-#[derive(Default, BinRead)]
-pub struct PartialParams {
-    magic: [u8; 8],
-    batch_size: i32,
-    segment_size: i32,
-    segment_step: i32,
-    mel_features: i32,
-    sample_rate: i32,
-
+    // Stride in milliseconds, for example 10
     frame_shift_ms: i32,
+
+    // Window length in milliseconds, for example 25
     frame_length_ms: i32,
-    round_pow2: i32,
-    mel_low: i32,
-    mel_high: i32,
-    snip_edges: i32,
 
-    token_count: i32,
-    blank_id: i32,
-}
+    // Number of mel bins, for example 80
+    num_bins: i32,
 
-#[derive(Debug)]
-pub struct Params {
-    batch_size: i32,
-    segment_size: i32,
-    segment_step: i32,
-    mel_features: i32,
-    sample_rate: i32,
-
-    frame_shift_ms: i32,
-    frame_length_ms: i32,
+    // Whether or not to round window size to pow2
     round_pow2: bool,
+
+    // Mel low frequency, for example 20 Hz
     mel_low: i32,
+
+    // Mel high frequency. If 0, sample_freq/2 is assumed
     mel_high: i32,
+
+    // ???????????
     snip_edges: bool,
 
-    blank_id: i32,
+    // How many segments to pull in fbank_pull_segments.
+    // For example, if this is equal to 9, then you should call
+    // fbank_pull_segments with a tensor of size (1, 9, num_bins)
+    pull_segment_count: i32,
 
-    networks: Vec<Network>,
-    tokens: Vec<String>,
+    // How many segments to step over in fbank_pull_segments.
+    // For example, if this is set to 4, then each call to fbank_pull_segments
+    // will step over 4 segments
+    pull_segment_step: i32,
+
+    // If false, speed feature will be unavailable
+    use_sonic: bool,
 }
 
-#[derive(Debug, Clone, Copy, BinRead)]
-pub struct Network {
-    offset: u64,
-    size: u64,
-}
-impl Network {
-    pub fn read<T>(self, cursor: &mut T) -> Result<Vec<u8>, Box<dyn Error>>
-    where
-        T: Read + Seek,
-    {
-        cursor.seek(std::io::SeekFrom::Start(self.offset))?;
-        let mut buf = vec![0; self.size as usize];
-        cursor.read_exact(&mut buf)?;
-        Ok(buf)
-    }
-}
+pub struct AprilASRModel<'a> {
+    env: Arc<Environment>,
+    encoder: InMemorySession<'a>,
+    decoder: InMemorySession<'a>,
+    joiner: InMemorySession<'a>,
 
-#[derive(Debug)]
-pub struct ModelFile {
-    version: u32,
-    language: String,
-    name: String,
-    description: String,
-    model_type: ModelType,
-    params: Params,
+    // same as eout_dim
+    x_dim: Vec<usize>,
+    h_dim: Vec<usize>,
+    c_dim: Vec<usize>,
+    dout_dim: Vec<usize>,
+    eout_dim: Vec<usize>,
+    context_dim: Vec<usize>,
+    logits_dim: Vec<usize>,
+    fbank_opts: FBankOptions,
+    // ModelParameters params;
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut cursor = fs::File::open("model.april")?;
-    let file_header: ModelFileHeader = cursor.read_ne()?;
-    let lang_header: LanguageHeader = cursor.read_ne()?;
-    let language = std::str::from_utf8(&lang_header.language)?
-        .trim_matches('\x00')
-        .to_string();
-    let name = read_string(&mut cursor)?;
-    let description = read_string(&mut cursor)?;
-
-    let model_type: ModelType = cursor.read_ne()?;
-    let params: ParamMetadata = cursor.read_ne()?;
-    let params = params.parse(&mut cursor)?;
-
-    let model_file = ModelFile {
-        version: file_header.version,
-        language,
-        name,
-        description,
-        model_type,
-        params,
-    };
-
+    let model_file = model_file::ModelFile::new(&mut cursor)?;
     assert_eq!(
         model_file.model_type,
-        ModelType::ModelLstmTransducerStateless
+        model_file::ModelType::ModelLstmTransducerStateless
     );
 
     let environment = Environment::builder()
@@ -232,35 +91,96 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .into_arc();
 
     // Here's some repeated boilerplate because we can't clone a session builder
-    let encoder = model_file.params.networks[0].read(&mut cursor)?;
     let encoder = ort::SessionBuilder::new(&environment)?
         .with_inter_threads(1)?
         .with_intra_threads(1)?
-        .with_model_from_memory(&encoder)?;
+        .with_model_from_memory(&model_file.params.networks[0])?;
 
     assert_eq!(encoder.inputs.len(), 3);
     assert_eq!(encoder.outputs.len(), 3);
 
-    let decoder = model_file.params.networks[1].read(&mut cursor)?;
     let decoder = ort::SessionBuilder::new(&environment)?
         .with_inter_threads(1)?
         .with_intra_threads(1)?
-        .with_model_from_memory(&decoder)?;
+        .with_model_from_memory(&model_file.params.networks[1])?;
 
     assert_eq!(decoder.inputs.len(), 1);
     assert_eq!(decoder.outputs.len(), 1);
 
-    let joiner = model_file.params.networks[2].read(&mut cursor)?;
     let joiner = ort::SessionBuilder::new(&environment)?
         .with_inter_threads(1)?
         .with_intra_threads(1)?
-        .with_model_from_memory(&joiner)?;
+        .with_model_from_memory(&model_file.params.networks[2])?;
 
     assert_eq!(joiner.inputs.len(), 2);
     assert_eq!(joiner.outputs.len(), 1);
 
-    // assert_eq!(encoder.inputs[0].dimensions, 3);
-    // assert_eq!(encoder.outputs.len(), 3);
+    let x_dim = encoder.inputs[0]
+        .dimensions()
+        .take(3)
+        .map(|c| c.unwrap())
+        .collect();
+    let h_dim = encoder.inputs[1]
+        .dimensions()
+        .take(3)
+        .map(|c| c.unwrap())
+        .collect();
+    let c_dim = encoder.inputs[2]
+        .dimensions()
+        .take(3)
+        .map(|c| c.unwrap())
+        .collect();
+    let eout_dim = encoder.outputs[0]
+        .dimensions()
+        .take(3)
+        .map(|c| c.unwrap())
+        .collect();
 
+    let context_dim = decoder.inputs[0]
+        .dimensions()
+        .take(2)
+        .map(|c| c.unwrap())
+        .collect();
+    let dout_dim = decoder.outputs[0]
+        .dimensions()
+        .take(3)
+        .map(|c| c.unwrap())
+        .collect();
+
+    let logits_dim = joiner.outputs[0]
+        .dimensions()
+        .take(3)
+        .map(|c| c.unwrap())
+        .collect();
+
+    let model = AprilASRModel {
+        env: environment,
+        encoder,
+        decoder,
+        joiner,
+        x_dim,
+        h_dim,
+        c_dim,
+        dout_dim,
+        eout_dim,
+        context_dim,
+        logits_dim,
+        fbank_opts: FBankOptions {
+            sample_freq: model_file.params.sample_rate,
+            frame_shift_ms: model_file.params.frame_shift_ms,
+            frame_length_ms: model_file.params.frame_length_ms,
+            num_bins: model_file.params.mel_features,
+            round_pow2: model_file.params.round_pow2,
+            mel_low: model_file.params.mel_low,
+            mel_high: model_file.params.mel_high,
+            snip_edges: true,
+            pull_segment_count: model_file.params.segment_size,
+            pull_segment_step: model_file.params.segment_step,
+            use_sonic: false,
+        },
+    };
+
+    assert_eq!(model.x_dim[0], model_file.params.batch_size as usize);
+    // assert_eq!(model.x_dim[0], model_file.params.batch_size as usize);
     Ok(())
 }
